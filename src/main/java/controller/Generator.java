30c1d8583ec2f4f9601f21be8007b4081a609788
michal <kuk.michal3@gmail.com>
2021-01-20 12:32:36 +0100

    add simple class structure

---
+++ src/main/java/controller/Generator.java
@@ -0,0 +1,9 @@
+package controller;
+
+//random password generator
+public class Generator {
+
+    public String generateRandomPassword(){
+        return  "";
+    }
+}
--- src/main/java/model/Email.java
+++ src/main/java/model/Email.java
@@ -1,4 +1,8 @@
 package model;
 
 public class Email {
+    private String firstName;
+    private String lastName;
+    private String department;
+    private String password;
 }
