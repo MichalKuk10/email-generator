aba82b42671ec402f78e39cbcf029d5fe110f7ae
michal <kuk.michal3@gmail.com>
2021-01-20 14:11:48 +0100

    add refactor

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        //it's for happy path
         System.out.println("Please provide new hire Name and Surname: ");
         Scanner in = new Scanner(System.in);
         String answer = in.nextLine();
